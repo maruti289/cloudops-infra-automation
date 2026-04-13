5bedbd4945a3b2a7f751f3c0378516cd30f66a2c
pmaruti289 <pmaruti289@gmail.com>
2026-04-14 01:38:56 +0530

    fix: update region eu-north-1, t3.micro, correct AMI

--- terra-config/variable.tf
+++ terra-config/variable.tf
@@ -1,5 +1,23 @@
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  default = "eu-north-1"
+}
+
+variable "ami_id" {
+  default = "PASTE_AMI_ID_HERE"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "environments" {
+  default = ["dev", "stage", "prod"]
+}
+
+variable "key_name" {
+  default = "tester-key"
+}
+
+variable "public_key_path" {
+  default = "~/.ssh/appKey.pub"
 }
